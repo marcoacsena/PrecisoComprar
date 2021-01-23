0eef75a0b8f42b1ae8a9ad554ca4e4662a61e761
marcoacsena <marcoacsena@gmail.com>
2021-01-23 18:53:40 -0300

    Commit inicial.

--- app/src/main/java/com/example/precisocomprar/network/RemoteDataSource.kt
+++ app/src/main/java/com/example/precisocomprar/network/RemoteDataSource.kt
@@ -1,15 +1,21 @@
 package com.example.precisocomprar.network
 
 import com.example.precisocomprar.model.LoginRequest
+import com.example.precisocomprar.util.FakeDataBase
 
 class RemoteDataSource {
 
     fun login(loginRequest : LoginRequest, onUserLoggedIn: (String?, Throwable?) -> Unit) {
 
         Thread{
-            //Código para simular um delay
-            Thread.sleep(2000)
-            onUserLoggedIn("atraso", null)
+            FakeDataBase.login(loginRequest){ res ->
+                if(res != null){
+                    onUserLoggedIn(res.token, null)
+                }else{onUserLoggedIn(null,null)}
+            }
+//            //Código para simular um delay
+//            Thread.sleep(2000)
+//            onUserLoggedIn("Atraso", null)
 
         }.start()
 
